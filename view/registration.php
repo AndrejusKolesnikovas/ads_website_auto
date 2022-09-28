689b70b32bb2b403355812b32f4d8ac6b53e5547
AndrejusKolesnikovas <andrejuskolesnikovas@yahoo.com>
2022-09-28 19:55:12 +0300

    registration form

---
+++ view/registration.php
@@ -0,0 +1,10 @@
+<h3>Register</h3>
+<form action="/registration" method="POST">
+    <div style="display: flex; flex-direction: column; width: 20vh">
+        <input type="email" name="email" placeholder="test@test.lt"/>
+        <input type="password" name="password" placeholder="password"/>
+        <input type="password" name="password_repeat" placeholder="Repeat password"/>
+        <input type="tel" name="phone_number" placeholder="Phone number"/>
+        <input type="submit"/>
+    </div>
+</form>
